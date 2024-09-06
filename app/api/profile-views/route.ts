d99bb6f347fa612c811b531ee2e71cf5771f8b7e
ajohnstondev <ajohnstondev@gmail.com>
2024-09-06 11:27:57 -0400

    fix build error

--- app/api/profile-views/route.ts
+++ app/api/profile-views/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  const startDate = request.nextUrl.searchParams || '2020-01-01';
-  const slug = request.nextUrl.pathname;
+  // const startDate = request.nextUrl.searchParams || '2020-01-01';
+  // const slug = request.nextUrl.pathname;
 
   return NextResponse.json({
     pageViews: 0,
